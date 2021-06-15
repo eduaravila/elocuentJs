640ac07a2ddb4e9eaa436ff710f4f762691529a2
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-06-14 20:21:53 -0500

    Update vectorType.js

--- vectorType.js
+++ vectorType.js
@@ -1,4 +1,26 @@
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-class Vector{
+  plus(_Vector) {
+    return new Vector(this.x + _Vector.x, this.y + _Vector.y);
+  }
 
+  minus(_Vector) {
+    return new Vector(this.x - _Vector.x, this.y - _Vector.y);
+  }
+
+  get length() {
+    // √(x2 + y2)
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
 }
+
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
