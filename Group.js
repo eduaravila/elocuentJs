61364d8be795320b9827d0caf93bbc2b434fc310
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-06-14 20:53:22 -0500

    Create Group.js

---
+++ Group.js
@@ -0,0 +1,44 @@
+class Group {
+  constructor() {
+    this.values = [];
+  }
+
+  _alreadyExist(val) {
+    return this.values.includes(val);
+  }
+
+  add(val) {
+    !this._alreadyExist(val) && this.values.push(val);
+  }
+
+  delete(val) {
+    if (this._alreadyExist(val)) {
+      let valInx = this.values.indexOf(val);
+      this.values = [
+        ...this.values.slice(0, valInx),
+        ...this.values.slice(valInx + 1),
+      ];
+    }
+  }
+
+  has(val) {
+    return this._alreadyExist(val);
+  }
+
+  static from(arr = []) {
+    let newGroup = new Group();
+    arr.map((i) => newGroup.add(i));
+    return newGroup;
+  }
+}
+
+let group = Group.from([10, 20]);
+
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
