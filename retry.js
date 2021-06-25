205bb3033c58bbc0a9c333e3fa7b46c0c44e2962
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-06-24 19:22:27 -0500

    Update retry.js

--- retry.js
+++ retry.js
@@ -1,2 +1,28 @@
 // 08
-"use strict"
+"use strict";
+
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) {
+  for (;;) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (error) {
+      if (error instanceof MultiplicatorUnitFailure) {
+        console.log("Probability error");
+      } else {
+        throw e;
+      }
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8));
