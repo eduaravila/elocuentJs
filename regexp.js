1c7edb3493f1cd70e7b375c46ee29136c489ff69
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-06-26 20:11:41 -0500

    Create regexp.js

---
+++ regexp.js
@@ -0,0 +1,45 @@
+// Fill in the regular expressions
+
+verify(/(car|cat)/, ["my car", "bad cats"], ["camper", "high art"]);
+
+verify(/(pop|prop)/gi, ["pop culture", "mad props"], ["plop", "prrrop"]);
+
+verify(
+  /(ferret|ferry|ferrari)/,
+  ["ferret", "ferry", "ferrari"],
+  ["ferrum", "transfer A"]
+);
+
+verify(
+  /ious\b/,
+  ["how delicious", "spacious room"],
+  ["ruinous", "consciousness"]
+);
+
+verify(/\s(\.|\,)/gi, ["bad punctuation ."], ["escape the period"]);
+
+verify(
+  /\w{6,}/,
+  ["Siebentausenddreihundertzweiundzwanzig"],
+  ["no", "three small words"]
+);
+
+verify(
+  /\b[^\We]+\b/i,
+  ["red platypus", "wobbling nest"],
+  ["earth bed", "learning ape", "BEET"]
+);
+
+function verify(regexp, yes, no) {
+  // Ignore unfinished exercises
+  if (regexp.source == "...") return;
+  for (let str of yes) {
+    if (!regexp.test(str)) {
+      console.log(`Failure to match '${str}'`);
+    }
+  }
+  for (let str of no)
+    if (regexp.test(str)) {
+      console.log(`Unexpected match for '${str}'`);
+    }
+}
