cc14d3367ef5933ace92283c62b00c990d7d725a
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-07-15 22:02:43 -0500

    Update graph.ts

--- datastructure/graph.ts
+++ datastructure/graph.ts
@@ -20,11 +20,11 @@ interface connection {
 const adjacencylistRepresentation = (
   nodes: string[],
   connections: Array<Array<string>>,
-  memo?: Map<string, object>
+  memo?: Map<string, Array<string>>
 ) => {
   memo = memo || new Map();
-  nodes.map((val) => {
-    memo.set(
+  nodes.map((val: string) => {
+    memo?.set(
       val,
       connections
         .filter(([key, direction], connectionInx) => key == val && direction)
@@ -35,4 +35,33 @@ const adjacencylistRepresentation = (
   return memo;
 };
 
+const bfshashconnection = (
+  list: Map<string, Array<string>>,
+  start: string,
+  target: string
+) => {
+  let queue: string[] = [start];
+  let visited = new Set();
+
+  while (queue.length > 0) {
+    let node = queue.shift()!;
+    let destinations = list.get(node)!;
+
+    for (let destination of destinations) {
+      if (destination == target) {
+        console.log(destination, "connection");
+      }
+
+      if (!visited.has(destination)) {
+        visited.add(destination);
+        queue.push(destination);
+      }
+    }
+  }
+};
+
 let graph = adjacencylistRepresentation(airports, routes);
+
+
+
+bfshashconnection(graph, "MEX", "LIM");
