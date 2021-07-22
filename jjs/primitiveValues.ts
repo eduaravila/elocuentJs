bfd6858a7e974ee6fd70e467198b4f5d246a3260
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-07-22 18:18:01 -0500

    Update primitiveValues.ts

---
+++ jjs/primitiveValues.ts
@@ -0,0 +1,22 @@
+console.log(undefined); // unintentionally missing values
+console.log(null); // intentionally missing values
+console.log(true); // user for logical operations
+console.log(100); // user for mathematical operations
+console.log(BigInt); // user for math or big numbers (uncommon)
+console.log("String"); // used for text
+console.log(Symbol.iterator); // used to perform rituals and hide secrets
+console.log({}); // used to group related data and code
+console.log([]); // used to group related data and code
+console.log((x: number) => x * 2); // used to refer to code
+
+
+
+
+console.log(typeof({})); // "object"
+console.log(typeof([])); // "object"
+console.log(typeof((x: number) => x * 22)); // "function"
+
+
+
+console.log(typeof(2)); // "number"
+// Expressions are questions that JavaScript can answer. JavaScript answers expressions in the only way it knows how—with values.
