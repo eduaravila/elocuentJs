5c247b289f4a6372bbbb30773711ce21b38254f1
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-07-26 21:42:39 -0500

    Update strictEquality.ts

--- jjs/strictEquality.ts
+++ jjs/strictEquality.ts
@@ -6,17 +6,25 @@
 const strictEquals = <Type>(a: Type, b: Type): boolean => {
   // same values
 
-  if (Object.is(a, b)) return true;
+  if (Object.is(a, b)) {
+    if (Object.is(a, NaN) && Object.is(b, NaN)) {
+      return true;
+    }
 
-  if (Object.is(a, NaN) && Object.is(b, NaN)) {
-    return true;
-  }
-
-  if (
-    (Object.is(a, -0) && Object.is(b, 0)) ||
-    (Object.is(a, 0) && Object.is(b, -0))
-  ) {
+    if (
+      (Object.is(a, -0) && Object.is(b, 0)) ||
+      (Object.is(a, 0) && Object.is(b, -0))
+    ) {
+      return false;
+    }
     return true;
   }
   return false;
 };
+
+console.log(strictEquals(0, -0));
+console.log(strictEquals(-0, -0));
+console.log(strictEquals(0, 0));
+console.log(strictEquals(-0, 0));
+console.log(strictEquals(NaN, NaN));
+console.log(strictEquals("S", "S"));
