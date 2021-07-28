9c2718171f408742ce039e4cc101c7d4055e54ce
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-07-27 23:50:23 -0500

    Update properties.ts

--- jjs/properties.ts
+++ jjs/properties.ts
@@ -24,3 +24,35 @@ console.log(holmes.surname); // Holmes
 console.log(holmes.address.city); // Malibu
 console.log(john.surname); // Lennon
 console.log(john.address.city); // Malibu
+
+let singer = { surname: "Turner" };
+let pilot = { surname: "Kamal" };
+let temp = singer.surname;
+singer.surname = pilot.surname;
+pilot.surname = temp;
+
+console.log(singer.surname); // "Kamal"
+console.log(pilot.surname); // "Turner"
+
+
+let president:any = {
+  name: 'Pooh',
+  next: null,
+};
+
+president.next = {
+  name: 'Paddington',
+  next: president,
+};
+
+console.log(president.next);
+
+let station = {
+    Owner: { name: 'Fred' },
+  };
+  //            undefined
+  //         |‾‾‾‾‾‾‾‾‾‾‾|  
+let name = station.owner.name;
+  //                      ^
+  //                    crash
+console.log(name === station.Owner.name);
